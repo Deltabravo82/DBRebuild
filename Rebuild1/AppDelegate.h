9a3e5e7548d5604f599b3e955959c23d691aaff5
William Langenbach <William@glorfindel.local>
2017-08-17 18:29:59 -0700

    Initial Commit

---
+++ Rebuild1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rebuild1
+//
+//  Created by William Langenbach on 8/17/17.
+//  Copyright © 2017 William Langenbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rebuild1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rebuild1
+//
+//  Created by William Langenbach on 8/17/17.
+//  Copyright © 2017 William Langenbach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
